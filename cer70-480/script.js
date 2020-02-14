d79b8553f1323db70d5187c0cc216c0d95e7fbbe
DaiNB-PC-056 <nguyen.ba.dai@kloon.vn>
2020-02-14 09:43:23 +0700

    add cer 70-480

---
+++ cer70-480/script.js
@@ -0,0 +1,9 @@
+/*
+
+ 
+*/
+
+//https://github.com/oscardo/Certification/blob/master/Exam%20Ref%2070-480%20Programming%20in%20HTML5%20with%20JavaScript%20and%20CSS3.pdf
+//http://prntscr.com/qx92lk
+
+//(game icon) https://vn.lovepik.com/image-610765036/dragon-boat-festival-cartoon-dice-theme-camera-icon.html
