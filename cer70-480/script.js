e4ba012eba234330dc0af529b3b425aaf3b38dd0
DaiNB-PC-056 <nguyen.ba.dai@kloon.vn>
2020-02-22 11:25:22 +0700

    update script questions

--- cer70-480/script.js
+++ cer70-480/script.js
@@ -1,5 +1,25 @@
 /*
 
+need to review: Objective 3.2: Validate user input by using JavaScript
+
+EXAM TIP
+The open method doesn't make any server requests. If the user name and password is specified, it doesn't send this information to the server in the open method. When the send method is called, the user name and password aren't passed to the server either. The credentials are passed to the server only in response to a 401 security response from the server
+
+unwieldy
+
+The readAsArrayBuffer method is used to read a File, Blob, MSStream into memory as an ArrayBuffer object
+Note:
+the pattern attr is supported in the IE10, Firefox, Opera, Chrome
+the pattern attr works with the following input types: text, search, url, tel, email, password
+
+=== is 'strict' or 'identical' equality
+
+Question 128
+
+
+
+
+ 
  
 */
 
@@ -7,3 +27,39 @@
 //http://prntscr.com/qx92lk
 
 //(game icon) https://vn.lovepik.com/image-610765036/dragon-boat-festival-cartoon-dice-theme-camera-icon.html
+//https://www.tutorialspoint.com/object-assign-in-javascript
+//https://viblo.asia/p/co-ban-ve-web-workers-ORNZqwpL50n
+//https://viblo.asia/p/tim-hieu-websocket-va-xay-dung-ung-dung-bidirectional-chat-n7prv3aVMKod
+
+
+//http://prntscr.com/r3e8sh
+//http://prntscr.com/r3zaj7
+//http://prntscr.com/r407le
+//http://prntscr.com/r4kkw6
+//http://prntscr.com/r4ktgl
+
+// Q.134 - http://prntscr.com/r5zna8
+// Q.136 - http://prntscr.com/r5zn5a
+// Q.138 - http://prntscr.com/r5zn10
+// Q.140 - http://prntscr.com/r5zmtv
+
+///https://api.jquery.com/on/
+// check jquery toggle
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
