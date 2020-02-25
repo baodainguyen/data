0addf22ead68b7eaf7519fc6bf801ef17b680b4d
Nguyen Bao Dai <dainb@Dais-McMini.local>
2020-02-25 22:24:16 +0700

    update

--- cer70-480/script.js
+++ cer70-480/script.js
@@ -1,9 +1,101 @@
 /*
 
+need to review: Objective 3.2: Validate user input by using JavaScript
+
+EXAM TIP
+The open method doesn't make any server requests. If the user name and password is specified, it doesn't send this information to the server in the open method. When the send method is called, the user name and password aren't passed to the server either. The credentials are passed to the server only in response to a 401 security response from the server
+
+unwieldy
+
+The readAsArrayBuffer method is used to read a File, Blob, MSStream into memory as an ArrayBuffer object
+Note:
+the pattern attr is supported in the IE10, Firefox, Opera, Chrome
+the pattern attr works with the following input types: text, search, url, tel, email, password
+
+=== is 'strict' or 'identical' equality
+
+http://www.daoudisamir.com/references/vs_ebooks/html5_css3.pdf
+
+
+ 
  
 */
 
-//https://github.com/oscardo/Certification/blob/master/Exam%20Ref%2070-480%20Programming%20in%20HTML5%20with%20JavaScript%20and%20CSS3.pdf
-//http://prntscr.com/qx92lk
-
 //(game icon) https://vn.lovepik.com/image-610765036/dragon-boat-festival-cartoon-dice-theme-camera-icon.html
+//https://www.tutorialspoint.com/object-assign-in-javascript
+//https://viblo.asia/p/co-ban-ve-web-workers-ORNZqwpL50n
+//https://viblo.asia/p/tim-hieu-websocket-va-xay-dung-ung-dung-bidirectional-chat-n7prv3aVMKod
+
+
+// http://prntscr.com/r3e8sh
+// http://prntscr.com/r3zaj7
+// http://prntscr.com/r407le
+
+// Q.134 - http://prntscr.com/r5zna8
+// Q.138 - http://prntscr.com/r5zn10
+// Q.151 - http://prntscr.com/r60795
+// Q.161 - http://prntscr.com/r60ezp
+// Q.165 - http://prntscr.com/r60kna
+// Q.167 - http://prntscr.com/r60nlu
+// Q.168 - http://prntscr.com/r60o0f
+// Q.169 - http://prntscr.com/r60oc9
+// Q.171 - http://prntscr.com/r60ov4
+// Q.175 - http://prntscr.com/r60qgn
+
+
+//       - http://prntscr.com/r6u5a3
+//       - http://prntscr.com/r75msw
+// Q.80  - http://prntscr.com/r75qru
+// Q.93  - http://prntscr.com/r767g1
+// Q.109 - http://prntscr.com/r774yu
+// Q.112 - http://prntscr.com/r776ct
+// Q.115 - http://prntscr.com/r777af
+// Q.118 - http://prntscr.com/r77au4
+// Q.132 - http://prntscr.com/r7b8ys        (websocket)
+// Q.155 - http://prntscr.com/r7cbuf        css: div ~ a
+// The box-sizing CSS property sets how the total width and height of an element is calculated.
+// Q.176 - http://prntscr.com/r7cwdt        parseInt('12px', 10) 
+// Q.198 - http://prntscr.com/r7du13
+// Q.200 - http://prntscr.com/r7dvkr        transform: rate(90deg)
+// Q.210 - http://prntscr.com/r7eb7h        ajax contentType: 'application/soap+xml', dataType: 'xml'
+// Q.211 - http://prntscr.com/r7edd3        display: flex
+// Q.212 - http://prntscr.com/r7ef1l        decodeURIComponent(str) in query string display @ symbol when using ajax
+// Q.223 - http://prntscr.com/r7exib        XMLHttpRequest;
+// Q.251 - http://prntscr.com/r7fnr5        ajax xml file
+
+
+
+///https://api.jquery.com/on/
+// check jquery toggle
+
+// $.ajax - dataType, contentType, accepts?
+
+
+// wrong Answer:
+// Q.147 - http://prntscr.com/r5zqr0
+// Q.148
+// Q.153
+// Q.156
+// Q.181 - http://prntscr.com/r60t7w
+
+// Q.95  - http://prntscr.com/r76uqd
+
+// http://prntscr.com/r61cwd
+
+
+
+
+
+// Check done:
+// http://prntscr.com/r6t6k1
+
+
+
+
+
+
+
+
+
+
+
